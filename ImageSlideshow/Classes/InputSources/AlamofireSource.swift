18df281cd0e9b928a3ef0102211831b2536f9698
Sandin <sandin.dulic@allaboutapps.at>
2020-03-10 14:14:55 +0100

    add token to the request of afimage

--- ImageSlideshow/Classes/InputSources/AlamofireSource.swift
+++ ImageSlideshow/Classes/InputSources/AlamofireSource.swift
@@ -18,25 +18,30 @@ import AlamofireImage
 public class AlamofireSource: NSObject, InputSource {
     /// url to load
     public var url: URL
-    
+    public var authToken: String?
+
     /// placeholder used before image is loaded
     public var placeholder: UIImage?
 
     /// Initializes a new source with a URL
     /// - parameter url: a url to load
+    /// - parameter token: auth token
     /// - parameter placeholder: a placeholder used before image is loaded
-    public init(url: URL, placeholder: UIImage? = nil) {
+    public init(url: URL, token: String? = nil, placeholder: UIImage? = nil) {
         self.url = url
+        self.token = token
         self.placeholder = placeholder
         super.init()
     }
 
     /// Initializes a new source with a URL string
     /// - parameter urlString: a string url to load
+    /// - parameter token: a authToken
     /// - parameter placeholder: a placeholder used before image is loaded
-    public init?(urlString: String, placeholder: UIImage? = nil) {
+    public init?(urlString: String, token: String? = nil, placeholder: UIImage? = nil) {
         if let validUrl = URL(string: urlString) {
             self.url = validUrl
+            self.token = token
             self.placeholder = placeholder
             super.init()
         } else {
@@ -45,18 +50,35 @@ public class AlamofireSource: NSObject, InputSource {
     }
 
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
-        imageView.af_setImage(withURL: self.url, placeholderImage: placeholder, filter: nil, progress: nil) { [weak self] (response) in                                                              
-            switch response.result {
-            case .success(let image):
-                callback(image)
-            case .failure(_):
-                if let strongSelf = self {
-                callback(strongSelf.placeholder)
-            } else {
-                callback(nil)
-            }                                                                                                 
+        if token != nil {
+            var urlRequest = URLRequest(url: self.url)
+            urlRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+            imageView.af_setImage(withURLRequest: urlRequest, placeholderImage: self.placeholder, filter: nil, progress: nil) { [weak self] response in
+                switch response.result {
+                case .success(let image):
+                    callback(image)
+                case .failure:
+                    if let strongSelf = self {
+                        callback(strongSelf.placeholder)
+                    } else {
+                        callback(nil)
+                    }
+                }
+            }
+        } else {
+            imageView.af_setImage(withURL: self.url, placeholderImage: self.placeholder, filter: nil, progress: nil) { [weak self] response in
+                switch response.result {
+                case .success(let image):
+                    callback(image)
+                case .failure:
+                    if let strongSelf = self {
+                        callback(strongSelf.placeholder)
+                    } else {
+                        callback(nil)
+                    }
+                }
+            }
         }
-    }
     }
 
     public func cancelLoad(on imageView: UIImageView) {
